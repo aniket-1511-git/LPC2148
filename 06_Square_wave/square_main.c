65667f1de3710395bbc6e1536687c25f3c11e637
aniket omanna <aniket.omanna15@gmail.com>
2025-08-13 16:08:43 +0530

    Add 06_Squre_wave

--- 06_Square_wave/square_main.c
+++ 06_Square_wave/square_main.c
@@ -13,9 +13,13 @@ int main(void)
 	IODIR1 |= sqWave_u8;
 	while(1)
 	{
+		// setting  p1.16 pin
 		IOSET1 |= sqWave_u8;
+		// function call to generate 50 micros delay 
 		usec_delay(50);
+		// clearing p1.16 pin
 		IOCLR1 |= sqWave_u8;
+		// function call to generate 50 micros delay 
 		usec_delay(50);
 	}
 }
