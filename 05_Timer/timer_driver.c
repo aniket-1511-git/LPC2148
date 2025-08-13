99f67153a82eff547f945a1e780567bcb827d608
aniket omanna <aniket.omanna15@gmail.com>
2025-08-13 15:24:09 +0530

    Add 05_timer

---
+++ 05_Timer/timer_driver.c
@@ -0,0 +1,71 @@
+#include "timer_header.h" 
+
+
+/*
+* Function name      : void msec_delay(uint32_t ms_u32)
+* Description        : this function generate the delay in milliseconds
+*                      
+*/
+void msec_delay(uint32_t ms_u32)
+{
+	
+	// Array storing possible Peripheral Clock (PCLK) values in MHz
+	int32_t vpbdiv[] = {15,60,30,15};
+	// Set the mode as Timer
+	T0CTCR = 0;
+	// store max time based on VPBDIV setting
+	T0PR = vpbdiv[VPBDIV]*1000-1;
+	// Clear timer 
+	T0TC = 0;
+	// start timer 
+	T0TCR = 1;
+	// Wait untill t0tc value reaches  ms_u32
+	while(T0TC<ms_u32);
+	// stop timer 
+	T0TCR = 0 ;	
+}
+/*
+* Function name      : void sec_delay(uint32_t sec_u32)
+* Description        : this function generate the delay in seconds
+*                      
+*/
+void sec_delay(uint32_t sec_u32)
+{
+	// Array storing possible Peripheral Clock (PCLK) values in MHz
+	int32_t vpbdiv[] = {15,60,30,15};
+	// Set the mode as Timer
+	T0CTCR = 0;
+	// store max time based on VPBDIV setting
+	T0PR = vpbdiv[VPBDIV]*1000000-1;
+	// Clear timer 
+	T0TC = 0;
+	// start timer 
+	T0TCR = 1;
+	// Wait untill t0tc value reaches  sec_u32
+	while(T0TC<sec_u32);
+	// stop timer 
+	T0TCR = 0 ;
+}
+/*
+* Function name      : void usec_delay(uint32_t us_u32)
+* Description        : this function generate the delay in microseconds
+*                      
+*/
+void usec_delay(uint32_t us_u32)
+{
+	// Array storing possible Peripheral Clock (PCLK) values in MHz
+	int32_t vpbdiv[] = {15,60,30,15};
+	// Set the mode as Timer
+	T0CTCR = 0;
+	// store max time based on VPBDIV setting
+	T0PR = vpbdiv[VPBDIV]-1;
+	// Clear timer 
+	T0TC = 0;
+	// start timer 
+	T0TCR = 1;
+	// Wait untill t0tc value reaches  us_u32
+	while(T0TC<us_u32);
+	// stop timer 
+	T0TCR = 0 ;
+}
+
---
+++ 05_Timer/timer_main.c
@@ -0,0 +1,19 @@
+#include "timer_header.h" 
+/* 
+* Function name     : int main(void)
+* Description       : this function calling different delay functions
+*                      
+*/
+
+int main()
+{
+	// function call to generate delay in millisec 
+	// msec_delay(500);
+	// function call to generate delay in sec 
+	// sec_delay(2);
+	// function call to generate delay in microsec 
+	   usec_delay(500);
+	
+}
+
+
