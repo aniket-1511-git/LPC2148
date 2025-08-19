e42098e8f18d45d8d4135a2333241bca9d271361
aniket omanna <aniket.omanna15@gmail.com>
2025-08-19 11:41:58 +0530

    add 11 SPI Communication

--- 11_SPI_Communication/Objects/spi_main.d
+++
@@ -1,4 +0,0 @@
-.\objects\spi_main.o: spi_main.c
-.\objects\spi_main.o: spi_header.h
-.\objects\spi_main.o: C:\Keil_v5\ARM\INC\Philips\LPC214X.H
-.\objects\spi_main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
--- 11_SPI_Communication/Objects/spi_uart_driver.d
+++
@@ -1,4 +0,0 @@
-.\objects\spi_uart_driver.o: SPI_Uart_driver.c
-.\objects\spi_uart_driver.o: spi_header.h
-.\objects\spi_uart_driver.o: C:\Keil_v5\ARM\INC\Philips\LPC214X.H
-.\objects\spi_uart_driver.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
