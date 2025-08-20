34853205e3ff8347b003cb19d349b54f8d71120b
aniket omanna <aniket.omanna15@gmail.com>
2025-08-20 18:14:33 +0530

    Add 13_Worksheet_2148/01_LED_blink

--- 13_Worksheet_2148/01_LED_blink/led_header.h
+++ 13_Worksheet_2148/01_LED_blink/led_header.h
@@ -1,10 +1,14 @@
 #include <LPC214X.H>
 #include <stdint.h>
-
+/*Macro for LED */
 #define LED_u8 0Xf
+/*Macro for LED1 */
 #define LED1_u8 1<<0
+/*Macro for LED2 */
 #define LED2_u8 1<<1
+/*Macro for LED3 */
 #define LED3_u8 1<<2
+/*Macro for LED4 */
 #define LED4_u8 1<<3
 
-void sec_delay(uint32_t sec_u32);
+void sec_delay(uint32_t sec_u32); // Function to Delay generation 
--- 13_Worksheet_2148/01_LED_blink/led_main.c
+++ 13_Worksheet_2148/01_LED_blink/led_main.c
@@ -1,19 +1,33 @@
 #include "led_header.h"
-
-int main()
+/*
+* Function name      : int main(void)
+* Description        : this function blink LED's in Pattern
+*                      
+*/
+int main(void)
 {
+	// set Output direction to p0.0 to p0.3
 	IODIR0 |= LED_u8;
-	IOSET0 |= LED_u8;
+
 	while(1U)
 	{
+		// On led 1		
 		IOSET0 |= LED1_u8;
+		// Function Call to generate Delay
 		sec_delay(1);
+		// On led 2
 		IOSET0 |= LED2_u8;
+		// Function Call to generate Delay
 		sec_delay(1);
+		// On led 3
 		IOSET0 |= LED3_u8;
+		// Function Call to generate Delay
 		sec_delay(1);
+		// On led 4
 		IOSET0 |= LED4_u8;
+		// Function Call to generate Delay
 		sec_delay(1);
+		// Off All LED's
 		IOCLR0 = LED_u8;
 	}
 	
