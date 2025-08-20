da1238b7a5f1a71605b064973a6f567ffef8095c
aniket omanna <aniket.omanna15@gmail.com>
2025-08-20 14:25:13 +0530

    Add 01_LED_BLINK

--- 01_LED_BLINK/main.c
+++ 01_LED_BLINK/main.c
@@ -1,18 +1,33 @@
 #include <LPC214X.H>
+// Macro define for P0.14, P0.15 and P0.16
 #define LED 7<<14
+/*
+* Function Name : void delay(unsigned int ms)
+* Decription    : This Function generates software delay
+*/
 void delay(unsigned int ms)
 {
+	// Loop to iterate till delay time         
 	for(unsigned int i=0;i<ms;i++);
 }
-int main()
+/*
+* Function Name : int main(void)
+* Decription    : This Function blink the led after 100000 ms delay 
+*/
+int main(void)
 {
+	// Select output direction for LED
 	IODIR0 |= LED;
 	
 	while(1)
 	{
+		// Set the LED pins
 		IOSET0 |= LED;
+		// Function call to generate delay
 		delay(100000);
+		// Clear the LED Pins
 		IOCLR0 |= LED;
+		// Function call to generate delay
 		delay(100000);
 	}
 }
