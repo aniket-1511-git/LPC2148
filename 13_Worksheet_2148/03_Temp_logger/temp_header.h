2a5cb36b879f609a982590f8fbc5e4a243e4a329
aniket omanna <aniket.omanna15@gmail.com>
2025-08-29 11:07:51 +0530

    Add function descriptions

--- 13_Worksheet_2148/03_Temp_logger/temp_header.h
+++ 13_Worksheet_2148/03_Temp_logger/temp_header.h
@@ -17,24 +17,24 @@
 
 //=================== LCD Function prototype ===================
 
-void lcd_data(unsigned char data);
-void lcd_cmd(unsigned char data);
-void lcd_init(void);
-void lcd_str(char *p);
-void lcd_intNo(int no);
+void lcd_data(unsigned char data);    // Function to send char on LCD
+void lcd_cmd(unsigned char data);   // Function to Send command on LCD
+void lcd_init(void);   // Function to  Init LCD
+void lcd_str(char *p);   // Function to send String on LCD
+void lcd_intNo(int no);   // Function to send integer no  on LCD
 
 //=================== delay Function prototype ===================
 
-void ms_delay(int ms);
+void ms_delay(int ms);   // Function to generate delay
 
 //=================== UART Function prototype ===================
 
-void uart_init(uint16_t );            	  
-void uart_charTransmit(uint8_t);          
-uint8_t uart_charReceive(void);           
-void uart_stringTransmit(uint8_t *);	    
-void uart_stringReceive(uint8_t *,uint8_t);
+void uart_init(uint16_t );   // Function to  init uart        	  
+void uart_charTransmit(uint8_t);// Function to send on char uart           
+uint8_t uart_charReceive(void); // Function to read from  uart           
+void uart_stringTransmit(uint8_t *);// Function to send string on uart 	    
+void uart_stringReceive(uint8_t *,uint8_t);// Function to read string uart
 
 //=================== UART Function prototype ===================
-void adc_init(void);
-uint16_t adc0_read_data(uint8_t chNo_u8);
+void adc_init(void);// Function to init adc
+uint16_t adc0_read_data(uint8_t chNo_u8);// Function to read the data from uart
--- 13_Worksheet_2148/03_Temp_logger/temp_main.c
+++ 13_Worksheet_2148/03_Temp_logger/temp_main.c
@@ -1,16 +1,27 @@
 #include "temp_header.h"
-
-int main()
+/*
+* Function Name :int main(void)
+* Description   : This function read the analog data and display on LCD
+*/
+int main(void)
 {
+	// Varriable to store adc value and temperatur
 	uint16_t adcValue,temp;
+	// LCD init
 	lcd_init();
+	// UART init 
 	uart_init(9600);
+	// ADC init
 	adc_init();
 	while(1U)
 	{
+		// Send command to start form 1st line of LCD
 		lcd_cmd(0x80);
+		// Read and store data form ADC
 		adcValue = adc0_read_data('0');
+		// ADC value to temperature conversion formula
 		temp = ((adcValue *3.3)/1024)*100;
+		// Send message to LCD
 		lcd_str("Temp : ");
 		lcd_intNo(temp);
 		lcd_str(" 'c");
