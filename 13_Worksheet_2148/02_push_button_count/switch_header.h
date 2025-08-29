7e1cb380536c4dd88a2a8e36015eaddebeb10514
aniket omanna <aniket.omanna15@gmail.com>
2025-08-29 11:07:06 +0530

    Add 13_Worksheet_2148/02_push_button_count

--- 13_Worksheet_2148/02_push_button_count/switch_header.h
+++ 13_Worksheet_2148/02_push_button_count/switch_header.h
@@ -1,15 +1,20 @@
 #include <LPC214X.H>
 
+/* defined macro for Register select pin of LCD */
 #define RS (1<<17)
+/* defined macro for read write pin of LCD */
 #define RW (1<<18)
+/* defined macro for Enable pin of LCD */
 #define EN (1<<19)
 
+/* defined macro for Switch  */
 #define sw1 ((IOPIN0>>14)&1)
-void lcd_data(unsigned char data);
-void lcd_cmd(unsigned char data);
-void lcd_init(void);
-void lcd_str(char *p);
-void lcd_intNo(int no);
 
-void ms_delay(int ms);
+void lcd_data(unsigned char data);    // Function to send char on LCD
+void lcd_cmd(unsigned char data);   // Function to Send command on LCD
+void lcd_init(void);   // Function to  Init LCD
+void lcd_str(char *p);   // Function to send String on LCD
+void lcd_intNo(int no);   // Function to send integer no  on LCD
+
+void ms_delay(int ms);   // Function to generate delay
 
